612d481f212358b96c708080e1ff83d737ec688b
ahadhasanjion <niallhoran3176@gmail.com>
2022-08-23 18:27:51 +0600

    budget

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-
 function getInputFieldbyId(Id) {
-    const inputFeild = document.getElementById(Id);
-    const inputFieldValueString = inputFeild.value;
-    const inputFieldValue = parseInt(inputFieldValueString);
+    const inputField = document.getElementById(Id);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
     return inputFieldValue;
 }
 document.getElementById('calculate').addEventListener('click', function () {
+
     let perPlayerBudget = getInputFieldbyId('per-player');
-    let playerBudget = perPlayerBudget * playername.length; 
+    let playerBudget = perPlayerBudget * playername.length;
     const playerExpense = document.getElementById('player-Expense')
     playerExpense.innerText = playerBudget;
-    // console.log(playerExpense);
 })
 document.getElementById('calculate-total').addEventListener('click', function () {
     let perPlayerBudget = getInputFieldbyId('per-player');
-    let playerBudget = perPlayerBudget * playername.length; 
+    let playerBudget = perPlayerBudget * playername.length;
 
     let managerExpense = getInputFieldbyId('manager');
     let coachExpense = getInputFieldbyId('coach');
